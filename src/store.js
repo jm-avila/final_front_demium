da267ec9e33d156c86619fc3b4206e9144f7b9f1
jm-avila <jm.avila1309@icloud.com>
2019-03-08 09:05:56 +0100

    hola

--- src/store.js
+++ src/store.js
@@ -7,10 +7,7 @@ const initialState = {};
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(thunk))
 );
 
 export default store;
